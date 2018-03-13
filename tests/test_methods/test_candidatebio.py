3077b2302435b95771054277d9724b7d08d0ed9a
Danielsen <nathan.danielsen@gmail.com>
2018-03-13 11:46:00 -0400

    fix merge error

--- tests/test_methods/test_candidatebio.py
+++ tests/test_methods/test_candidatebio.py
@@ -3,3 +3,12 @@ from votesmart.methods.candidatebio import *
 
 def test_CandidateBio():
     method = CandidateBio(api_instance='test')
+
+
+def test_Bio():
+    d = {"candidate": {"items": 'ok', "something": 'value'}, 'story': "blah"}
+    obj = Bio(d)
+
+
+def test_AddlBio():
+    AddlBio({"candidate": 'hello'})
